a8468748437d350fe76d8940b3beb77453574ba4
Vikky-V <vivekraj.offcial@gmail.com>
2024-11-01 23:19:45 +0530

    Adds a score counter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+
+    private int score = 0;
     private float movementX;
     private float movementY;
     public float speed = 10;
+
+    public TextMeshProUGUI countText;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        score = 0;
+        setCountText();
     }
 
     public void FixedUpdate(){
@@ -26,6 +33,12 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter(Collider other){
         if(other.gameObject.CompareTag("PickUp")){
             other.gameObject.SetActive(false);
+            score += 1;
+            setCountText();
         }
+    }
+
+    void setCountText (){
+        countText.text = "Count: " + score.ToString();
     }
 }
